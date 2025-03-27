f9ddee70d32447b379a8a82107fe66ff32cb0b80
Isaac <a02376833@aggies.usu.edu>
2025-03-26 19:10:18 -0600

    please lord be last change

--- app/jobs/[slug]/page.tsx
+++ app/jobs/[slug]/page.tsx
@@ -5,8 +5,9 @@ import { getJobBySlug } from "@/lib/data"
 import { ArrowLeft, Calendar, MapPin } from "lucide-react"
 import "../job-detail.css"
 
-export default async function JobPage({ params }: { params: { slug: string } }) {
-  const job = await getJobBySlug(params.slug)
+export default async function JobPage({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params
+  const job = await getJobBySlug(slug)
 
   if (!job) {
     notFound()
